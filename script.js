617f3715e9f1572ccd55dd3ef6d768bd4d5439d1
Stephen Ochieng <stephenojwang1040@gmail.com>
2019-09-24 08:08:41 +0300

    handle text field

---
+++ script.js
@@ -0,0 +1,9 @@
+// DOM ELEMENT REFERENCES
+const firstNameInput = document.getElementById('first-name');
+const lastNameInput = document.getElementById('last-name');
+const submitButton = document.getElementById('submit-button');
+const sidebar = document.getElementById('sidebar');
+
+submitButton.addEventListener('click', () => {
+  sidebar.textContent = 'Hi there, ' + firstNameInput.value + ' ' + lastNameInput.value;
+});
