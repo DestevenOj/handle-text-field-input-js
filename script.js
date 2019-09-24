56757afbeb9347b51fd1caad752ede62ec0bf0a5
Stephen Ochieng <stephenojwang1040@gmail.com>
2019-09-24 08:21:51 +0300

    "fghj"

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const firstNameInput = document.getElementById('first-name');
 const lastNameInput = document.getElementById('last-name');
 const submitButton = document.getElementById('submit-button');
 const sidebar = document.getElementById('sidebar');
+const commentForm = document.querySelector('form');
 
-submitButton.addEventListener('click', () => {
+submitButton.addEventListener('click', ($event) => {
+//   prevent the submit button from refreshing the page.
+  $event.preventDefault(); 
   sidebar.textContent = 'Hi there, ' + firstNameInput.value + ' ' + lastNameInput.value;
+//  reset the form after it has been submitted.  
+  commentForm.reset();
 });
